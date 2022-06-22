7eb1954eff53d0b5f93627271774640233710304
new92 <94779840+new92@users.noreply.github.com>
2022-06-22 19:17:37 +0300

    Update PasswordGenerator.py

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -2,8 +2,11 @@
 Author: @new92
 Password Generator Program
 """
+
 import random
 import time
+
+#Main Program
 
 caps=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 lows=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
@@ -60,3 +63,5 @@ for i in range(length):
     psw += random.choice(RANDLIST)
 
 print("Password: "+str(psw))
+
+#End of the Program
