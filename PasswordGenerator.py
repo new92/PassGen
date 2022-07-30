b4ea715efff60c354210a9ce9fcddf7bc41909a9
new92 <94779840+new92@users.noreply.github.com>
2022-07-30 19:53:24 +0300

    Update PasswordGenerator.py

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,53 +1,57 @@
 """
 Author: @new92
-Password Generator Program
+Password Generator
+Program for Generating Strong Passwords !
 """
 
+#Imports
 import random
 import time
 
-#Main Program
-
+#Lists
 caps=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 lows=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 nums=["1","2","3","4","5","6","7","8","9"]
 spesChars=["!","@","#","$","%","^","&","*","|","/"]
-cap=input("Do you want to include cappital letters ?[Y/N] ")
-while cap != "y" and cap != "Y" and cap != "n" and cap != "N":
-    print("Invalid parameter !")
-    cap=input("\nPlease enter again: ")
+
+#Main Program 
+cap=input("[?] Do you want to include cappital letters ?[Y/N] ")
+while cap != "y" and cap != "Y" and cap != "n" and cap != "N" or cap == None:
+    print("[!] Invalid Input !")
+    cap=input("\n [::] Please enter again: ")
 if cap == "y" or cap == "Y":
     capital=True
 else:
     capital=False
-low=input("\nDo you want to include low letters ?[Y/N] ")
-while low != "y" and low != "Y" and low != "n" and low != "N":
-    print("Invalid parameter")
-    low=input("\nPlease enter again: ")
+low=input("\n [?] Do you want to include low letters ?[Y/N] ")
+while low != "y" and low != "Y" and low != "n" and low != "N" or low == None:
+    print("[!] Invalid Input !")
+    low=input("\n[::] Please enter again: ")
 if low == "y" or low == "Y":
     lower=True
 else:
     lower = False
-num=input("\nDo you want to include numbers ?[Y/N] ")
-while num != "y" and num != "Y" and num != "n" and num != "N":
-    print("Invalid parameter")
-    num=input("\nPlease enter again: ")
+num=input("\n [?] Do you want to include numbers ?[Y/N] ")
+while num != "y" and num != "Y" and num != "n" and num != "N" or num == None:
+    print("[!] Invalid Input !")
+    num=input("\n [::] Please enter again: ")
 if num == "y" or num == "Y":
     numbers=True
 else:
     numbers = False
-speschar=input("\nDo you want to include special characters ?[Y/N] ")
-while speschar != "y" and speschar != "Y" and speschar != "n" and speschar != "N":
-    print("Invalid parameter")
-    speschar=input("\nPlease enter again: ")
+speschar=input("\n[?] Do you want to include special characters ?[Y/N] ")
+while speschar != "y" and speschar != "Y" and speschar != "n" and speschar != "N"  or speschar == None:
+    print("[!] Invalid Input !")
+    speschar=input("\n[::] Please enter again: ")
 if speschar == "y" or speschar == "Y":
     specialchar = True
 else:
     specialchar = False
-length=int(input("Please enter the password length: "))
-while length < 8:
-    print("Too Short Password Length")
-    length=int(input("Please enter the password length: "))
+length=int(input("\n[::] Please enter the password length: "))
+while length < 4:
+    print("[!] Cannot generate password with that length !")
+    time.sleep(1)
+    length=int(input("\n[::] Please enter again the password length: "))
 
 RANDLIST = []
 if capital:
@@ -62,6 +66,4 @@ psw = ""
 for i in range(length):
     psw += random.choice(RANDLIST)
 
-print("Password: "+str(psw))
-
-#End of the Program
+print("\n[+] Your Password: "+str(psw))
