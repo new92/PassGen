03071d3f19ba7445cd1f0e653835d810cf00327a
new92 <94779840+new92@users.noreply.github.com>
2023-03-09 00:14:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from time import sleep
 import string
 
 caps=list(string.ascii_uppercase)
-lows=list(string.ascii_lowercase
+lows=list(string.ascii_lowercase)
 nums=["1","2","3","4","5","6","7","8","9"]
 spesChars=["!","@","#","$","%","^","&","*","|","/"]
 
