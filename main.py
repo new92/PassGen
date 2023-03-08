7da0d5221aed794aa12ea4ec867866ecc31f9b29
new92 <94779840+new92@users.noreply.github.com>
2023-03-09 00:46:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,24 +8,58 @@ Script for Generating strong passwords !
 from random import choice
 from time import sleep
 import string
+import os
 
 caps=list(string.ascii_uppercase)
 lows=list(string.ascii_lowercase)
 nums=[str(i) for i in range(10)]
 spesChars=["!","@","#","$","%","^","&","*","|","/"]
 
+def ProgInfo():
+    author = 'new92'
+    lice = 'MIT'
+    name = 'PassGen'
+    lang = 'Python'
+    language = 'en-US'
+    stars = 6
+    forks = 0
+    vers = 1.0
+    api = None
+    lines = 0
+    f = '/PassGen/main.py'
+    if os.path.exists(os.path.abs.path(f)):
+        fsize = (os.stat(f)).st_size
+    else:
+        fsize = 0
+    print(f"[+] Author: {author}")
+    print(f"[+] Github: @{author}")
+    print(f"[+] Version: {str(vers)}")
+    print(f"[+] Name: {name}")
+    print(f"[+] Programming language: {lang}")
+    print(f"[+] Natural language: {language}")
+    print(f"[+] Stars on Github repo: {str(stars)}")
+    print(f"[+] Forks on the Github repo: {str(forks)}")
+    print(f"[+] API used: {str(api)}")
+    print(f"[+] Number of lines: {lines}")
+    print(f"[+] File size: {str(fsize)} KB")
+    print(f"[+] Path to file: {os.path.abspath(f)}")
+
 def main():
     print("[+] Author: new92")
     print("[+] Github: @new92")
     print("\n")
     print("[1] Generate password")
-    print("[2] Exit")
+    print("[2] Display script's info")
+    print("[3] Exit")
     num=int(input("[::] Please enter a number (from the above ones): "))
-    while num < 1 or num > 2 or num == None:
+    while num < 1 or num > 3 or num == None:
         if num == None:
             print("[!] This field can't be blank !")
         else:
             print("[!] Invalid number !")
+        print("[1] Generate password")
+        print("[2] Display script's info")
+        print("[3] Exit")
         num=int(input("[::] Please enter again a number (from the above ones): "))
     if num == 1:
         cap=str(input("[?] Do you want to include cappital letters ? [Y/N] "))
@@ -88,11 +122,34 @@ def main():
             psw += choice(RANDLIST)
 
         print(f"[✓] Generated password: {psw}")
+        sleep(2)
+        print("[1] Return to menu")
+        print("[2] Exit")
+        number=int(input("[::] Please enter a number (from the above ones): "))
+        while number < 1 or number > 2 or number == None:
+            if number == None:
+                print("[!] This field can't be blank !")
+            else:
+                print("[!] Invalid number !")
+            print("[1] Return to menu")
+            print("[2] Exit")
+            number=int(input("[::] Please enter again a number (from the above ones): "))
+        if number == 1:
+            main()
+        else:
+            print("[+] Thank you for using my script 😁")
+            sleep(2)
+            print("[+] See you next time 👋")
+            sleep(1)
+            exit(0)
+    elif num == 2:
+        ProgInfo()
     else:
         print("[+] Thank you for using my script 😁")
         sleep(2)
         print("[+] See you next time 👋")
         sleep(1)
         exit(0)
+        
 if __name__ == '__main__':
     main()
